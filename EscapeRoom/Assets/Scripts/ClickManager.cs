9f1047e1629a121688852d7cc758b6340b86e0e6
Rich Chen <richcgold@gmail.com>
2020-07-15 14:31:01 -0700

    escape room prototype

---
+++ EscapeRoom/Assets/Scripts/ClickManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+        if (hit.collider != null) {
+            if (Input.GetMouseButtonDown(0)) {
+			    Debug.Log("Mouse Clicked");
+                Debug.Log(hit.collider.gameObject.name);
+                hit.collider.gameObject.GetComponent<MouseMove2D>().PickUp();
+		    }
+        }
+    }
+}
---
+++ EscapeRoom/Assets/Scripts/MouseMove2D.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class MouseMove2D : MonoBehaviour {
+ 
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.5f;
+    public bool isPickedUp = false;
+ 
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+    public void PickUp() {
+        isPickedUp = true;
+    }
+
+    public void Drop() {
+        isPickedUp = false;
+    }
+   
+    // Update is called once per frame
+    void Update () {
+        if (isPickedUp) {
+            if (Input.GetMouseButton(0)) {
+                mousePosition = Input.mousePosition;
+                mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+                transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+            } else {
+                this.Drop();
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "Square")
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
